9c98ae21304d0697ac30956b6f433a6d8b95ceee
Firoz Faruqui <firoz.f2003@gmail.com>
2023-09-27 01:48:09 +0530

    Adding Test component to test auto deployment git status

---
+++ src/main/java/com/lwc/devopsintegration/Test.java
@@ -0,0 +1,10 @@
+package com.lwc.devopsintegration;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Adding Test component to test auto deployment !!");
+
+	}
+
+}
